0bc4d55ea012180d19b814fa1f0d440282ceebf1
PeterM-13 <petershotbox@gmail.com>
2022-12-06 15:23:41 +0000

    order by added

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -1,7 +1,7 @@
 import { pool } from "../db/index.js";
 
 export async function getShoppingList() {
-  const data = await pool.query("SELECT * FROM shopping;");
+  const data = await pool.query("SELECT * FROM shopping ORDER BY completed;");
   console.log("The shopping list is", data.rows);
   return data.rows;
 }
